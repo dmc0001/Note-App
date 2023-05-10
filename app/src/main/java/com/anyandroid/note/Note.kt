e2987be67c7d25d71b7a03d55ee38f634273394d
Haithem Attab <lorenzomedici2001@gmail.com>
2023-05-10 15:31:25 +0200

    commit-note

---
+++ app/src/main/java/com/anyandroid/note/Note.kt
@@ -0,0 +1,6 @@
+package com.anyandroid.note
+
+data class Note (
+    val title: String,
+    val content: String
+)
---
+++ app/src/main/java/com/anyandroid/note/NoteAdapter.kt
@@ -0,0 +1,34 @@
+package com.anyandroid.note
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return NoteViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.bind(note)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        private val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+
+        fun bind(note: Note) {
+            titleTextView.text = note.title
+            contentTextView.text = note.content
+        }
+    }
+}
