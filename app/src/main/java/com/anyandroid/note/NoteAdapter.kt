8cb0ca96741d276674d3a3a2960764102d7911f7
Haithem Attab <lorenzomedici2001@gmail.com>
2023-05-11 18:42:05 +0200

    commit-note

--- app/src/main/java/com/anyandroid/note/NoteAdapter.kt
+++ app/src/main/java/com/anyandroid/note/NoteAdapter.kt
@@ -1,34 +1,63 @@
 package com.anyandroid.note
 
+
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+class NoteAdapter(
+    private val kindergartenList: ArrayList<Note>
+  //  , private val fragmentManager: FragmentManager
+) :
+    RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
-class NoteAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val noteTitle: TextView = itemView.findViewById(R.id.titleTextView)
+        val noteContent: TextView = itemView.findViewById(R.id.contentTextView)
+
+       // private var itemClickListener: OnItemClickListener? = null
+
+       /* init {
+            itemView.setOnClickListener {
+                itemClickListener?.onItemClick(adapterPosition)
+            }
+        }*/
+
+       /* fun setItemClickListener(itemClickListener: OnItemClickListener?) {
+            this.itemClickListener = itemClickListener
+        }*/
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
-        return NoteViewHolder(itemView)
+        val listItem = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        Log.d("NoteAdapter", "Creating ViewHolder")
+
+        return NoteViewHolder(listItem)
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        val note = notes[position]
-        holder.bind(note)
+        holder.noteTitle.text = kindergartenList[position].title
+        holder.noteContent.text = kindergartenList[position].content
+
+        /*holder.setItemClickListener(object : OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                // Launch the new fragment here and pass the uid as an argument
+                val uid =  kindergartenList[position].uid
+                val kindergartenName = kindergartenList[position].name
+                val newFragment = KindergartenFragment.newInstance(uid,kindergartenName)
+                fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView, newFragment)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        })*/
     }
 
     override fun getItemCount(): Int {
-        return notes.size
-    }
-
-    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-        private val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
-
-        fun bind(note: Note) {
-            titleTextView.text = note.title
-            contentTextView.text = note.content
-        }
+        return kindergartenList.size
     }
 }
